179b7912e53a499d3151c2475f8bf61a6595a80a
kmparxn <andreskmpa@gmail.com>
2023-03-24 10:40:36 -0500

    Agregar archivos de proyecto.

---
+++ ProyectoTest/Logica/Conexion.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProyectoTest.Logica
+{
+    public class Conexion
+    {
+        public static string CN = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=DB_CARRITO;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+    }
+}
